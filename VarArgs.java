30df1b83d047de938f45829835c79a648176c78e
Deeptansh Nagar <diptanshnagar@gmail.com>
2024-12-19 23:45:15 +0530

    func

--- VarArgs.java
+++ VarArgs.java
@@ -7,12 +7,25 @@ public class VarArgs {
         // fun(2, 3, 4, 5, 56, 87, 23, 45, 65);
         // fun(); // empty array.
 
-        multiple(2, 3, "Kunal", "Rahul", "Diptansh");
+        // multiple(2, 3, "Kunal", "Rahul", "Diptansh");
+        demo(2, 3, 4);
+        demo("kunal", "diptansh", "rohan cariappa");
+        // demo(); // this error is know as ambiguity.
+
+    }
+
+    static void demo(int ...v) {
+        System.out.println(Arrays.toString(v));
+    }
+
+    static void demo(String ...v) {
+        System.out.println(Arrays.toString(v));
     }
 
 
+
     static void multiple(int a, int b, String ...v) {
-        
+
     }
 
 
