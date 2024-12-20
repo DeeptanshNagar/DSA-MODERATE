321621f018a1b169aef087e92dda11f3db85e56e
Deeptansh Nagar <diptanshnagar@gmail.com>
2024-12-21 01:57:35 +0530

    Remove submodule and add 'functions' as regular folder

---
+++ functions/Overloading.java
@@ -0,0 +1,30 @@
+package functions;
+
+public class Overloading {
+    public static void main(String[] args) {
+        fun(67);
+        fun("Kunal Kushwaha");
+        int ans = sum(3,4);
+        int sol = sum(3,4,76);
+        System.out.println(ans);
+        System.out.println(sol);
+    }
+
+    static int sum(int a, int b) {
+        return a + b;
+    }
+
+    static int sum(int a, int b, int c) {
+        return a + b + c;
+    }
+
+    static void fun(int a) {
+        System.out.println("first one");
+        System.out.println(a);
+    }
+
+    static void fun(String name) {
+        System.out.println("second one");
+        System.out.println(name);
+    }
+}
