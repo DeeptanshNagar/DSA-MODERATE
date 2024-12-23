03d800c81be3e7e8e2a8e700a2f6c5f21620e394
Deeptansh Nagar <diptanshnagar@gmail.com>
2024-12-24 01:13:32 +0530

    rbs

---
+++ Binary Search/RotatedBS.java
@@ -0,0 +1,58 @@
+public class RotatedBS {
+    public static void main(String[] args) {
+        int[] arr = {4, 5, 6, 7, 0, 1, 2};
+        int target = 0;
+        System.out.println(search(arr, target));
+    }
+
+    static int search(int[] arr, int target) {
+        int pivot = findPivot(arr);
+        // If no pivot is found, the array is not rotated
+        if (pivot == -1) {
+            return binarySearch(arr, target, 0, arr.length - 1);
+        }
+        // If pivot is found, we have two ascending arrays
+        if (arr[pivot] == target) {
+            return pivot;
+        }
+        if (target >= arr[0]) {
+            return binarySearch(arr, target, 0, pivot - 1);
+        }
+        return binarySearch(arr, target, pivot + 1, arr.length - 1);
+    }
+
+    static int findPivot(int[] arr) {
+        int start = 0;
+        int end = arr.length - 1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (mid < end && arr[mid] > arr[mid + 1]) {
+                return mid;
+            }
+            if (mid > start && arr[mid] < arr[mid - 1]) {
+                return mid - 1;
+            }
+            if (arr[start] >= arr[mid]) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return -1;
+    }
+
+    static int binarySearch(int[] arr, int target, int start, int end) {
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (arr[mid] == target) {
+                return mid;
+            }
+            if (arr[mid] < target) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+        return -1;
+    }
+}
