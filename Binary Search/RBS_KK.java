84df087b33c14e4d911cd0d7b9d3295f790d6427
Deeptansh Nagar <diptanshnagar@gmail.com>
2024-12-24 15:24:35 +0530

    leetCode

--- Binary Search/RBS_KK.java
+++ Binary Search/RBS_KK.java
@@ -2,7 +2,9 @@
 
 public class RBS_KK {
     public static void main(String[] args) {
-        // int[] arr = {4, 5, 6, 7, 0, 1, 2};
+        int[] arr = {4, 5, 6, 7, 0, 1, 2};
+        // int target = 1;
+        // System.out.println(search(arr, target));
         // System.out.println(findPivot(arr));
     }
     static int search(int[] nums, int target) {
