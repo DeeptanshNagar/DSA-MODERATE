51206f78358628dbe66a2bf66ffe3c23c5ad8ebb
Deeptansh Nagar <diptanshnagar@gmail.com>
2025-01-02 03:51:33 +0530

    bubble

---
+++ Sorting Techniques/BubbleSort.java
@@ -0,0 +1,31 @@
+import java.util.Arrays;
+
+public class BubbleSort {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5};    
+        bubble(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    static void bubble(int[] arr) {
+        boolean swapped;
+        // run the steps n-1 times
+        for (int i = 0; i < arr.length; i++) {
+            swapped = false;
+            // for each step, max item will come at the last respective index position 
+            for (int j = 1; j < arr.length - i; j++) {
+                if (arr[j] < arr[j-1]) {
+                    // swap
+                    int temp = arr[j];
+                    arr[j] = arr[j-1];
+                    arr[j-1] = temp;
+                    swapped = true;
+                }
+            }
+            // if you didnt swap for a particular value of i, it means the array is sorted hence break
+            if (!swapped) {
+                break;
+            }
+        }
+    }
+}
