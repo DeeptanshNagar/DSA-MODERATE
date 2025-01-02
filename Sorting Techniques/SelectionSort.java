59aeb625cc0be5a2604004e5625830830f3142d6
Deeptansh Nagar <diptanshnagar@gmail.com>
2025-01-02 23:56:33 +0530

    ss

---
+++ Sorting Techniques/SelectionSort.java
@@ -0,0 +1,35 @@
+import java.util.Arrays;
+
+public class SelectionSort {
+    public static void main(String[] args) {
+        int[] arr = {4, 5, 1, 2, 3};
+        selection(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    static void selection(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            // find the max item in the remaining array and swap with correct index
+            int last = arr.length - i - 1;
+            int maxIndex = getMaxIndex(arr, 0, last);
+
+            swap(arr, maxIndex, last);
+        }
+    }
+
+    static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    static int getMaxIndex(int[] arr, int start, int end) {
+        int maxIndex = start;
+        for (int i = start; i <= end; i++) {
+            if (arr[i] > arr[maxIndex]) {
+                maxIndex = i;
+            }
+        }
+        return maxIndex;
+    }
+}
