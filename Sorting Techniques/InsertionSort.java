a214a3a8460c3e324a3eeddd901cd235121a756b
Deeptansh Nagar <diptanshnagar@gmail.com>
2025-01-03 23:34:54 +0530

    Is

---
+++ Sorting Techniques/InsertionSort.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+
+public class InsertionSort {
+    public static void main(String[] args) {
+        int[] arr = {5, 3, 4, 1, 2};
+        insertion(arr); 
+        System.out.println(Arrays.toString(arr)); // Print the sorted array
+    }
+
+    static void insertion(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = i + 1; j > 0; j--) {
+                if (arr[j] < arr[j - 1]) {
+                    swap(arr, j, j - 1); // Call the swap method
+                } else {
+                    break;
+                }
+            }
+        }
+    }
+
+    static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+}
