d8aadc58fb27205dd71f403e0825ae0df6184249
Deeptansh Nagar <diptanshnagar@gmail.com>
2025-01-11 00:45:44 +0530

    str

---
+++ Strings/Basic.java
@@ -0,0 +1,13 @@
+public class Basic {
+    public static void main(String[] args) {
+        int[] arr = {2, 3, 4, 5, 6};
+        int num = 10;
+        String name = "Kunal Kushwaha";
+        // System.out.println(name);
+
+        String a = "kunal";
+        System.out.println(a);
+        a = "kushwaha";
+        System.out.println(a);
+    }
+}
