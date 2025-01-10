08b372db219e70ccdfbb3e4cf48a3b74bef0b007
Deeptansh Nagar <diptanshnagar@gmail.com>
2025-01-11 04:05:06 +0530

    str

--- Strings/Output.java
+++ Strings/Output.java
@@ -1,8 +1,9 @@
-
-
 public class Output {
     public static void main(String[] args) {
         // System.out.println(56);
+        Integer num = new Integer(56); // wrapper class // treating integer as an object provides various methods which we can perform on variable num
+        System.out.println(num.toString());
+        System.out.println(num); // .toString is not neccessary to mention as it (num) will be by default refering to .valueOf and .valueOf is refering to .toString
         // System.out.println("Kunal");
         // System.out.println(Arrays.toString(new int[]{1, 2, 3, 4, 5}));
 
