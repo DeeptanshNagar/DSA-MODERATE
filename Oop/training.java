1c342a181c53d7d5b76b46ea8ca85ebdb67e13fb
Deeptansh Nagar <diptanshnagar@gmail.com>
2025-01-31 11:38:56 +0530

    practice

---
+++ Oop/training.java
@@ -0,0 +1,34 @@
+import java.util.Arrays;
+
+class Student {
+    int rno;
+    String name;
+    float marks;
+
+    // Constructor
+    Student(int rno, String name, float marks) {
+        this.rno = rno;
+        this.name = name;
+        this.marks = marks;
+    }
+
+    // To print student details properly
+    @Override
+    public String toString() {
+        return "Student{" + "rno=" + rno + ", name='" + name + '\'' + ", marks=" + marks + '}';
+    }
+}
+
+public class training {
+    public static void main(String[] args) {
+        // Initialize an array of students
+        Student[] students = new Student[2];
+
+        // Creating student objects
+        students[0] = new Student(1, "Kunal", 89.5f);
+        students[1] = new Student(2, "Alex", 76.2f);
+
+        // Print student array
+        System.out.println(Arrays.toString(students));
+    }
+}
