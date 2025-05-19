de6a3381d82f8e78a6954a93e2d265adb45512e3
Diptansh <diptanshnagar@gmail.com>
2025-05-19 22:11:30 +0530

    Update CyclicSort.java

--- Sorting Techniques/CyclicSort.java
+++ Sorting Techniques/CyclicSort.java
@@ -17,7 +17,6 @@ public class CyclicSort {
             }
         }
     }
-
     private static void swap(int[] arr, int first, int second) {
         int temp = arr[first];
         arr[first] = arr[second];
