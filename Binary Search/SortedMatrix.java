515151a28a7ce78351fef1b9a814e1504e03e7ee
Deeptansh Nagar <diptanshnagar@gmail.com>
2025-06-02 03:21:54 +0530

    dsa

--- Binary Search/SortedMatrix.java
+++ Binary Search/SortedMatrix.java
@@ -3,9 +3,10 @@ import java.util.Arrays;
 public class SortedMatrix {
     public static void main(String[] args) {
         int[][] arr = {
-                {1, 2, 3},
-                {4, 5, 6},
-                {7, 8, 9}
+                {1, 2, 3, 4},
+                {5, 6, 7, 8},
+                {9, 10, 11, 12},
+                {13, 14, 15, 16}
         };
         System.out.println(Arrays.toString(search(arr, 9))); // Output: [2, 2]
         System.out.println(Arrays.toString(search(arr, 5))); // Output: [1, 1]
