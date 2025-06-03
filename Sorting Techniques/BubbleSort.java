a5ed33cafe3008ee5612133c43db96ce38ff1abe
Deeptansh Nagar <diptanshnagar@gmail.com>
2025-06-03 22:22:46 +0530

    dsa

--- Sorting Techniques/BubbleSort.java
+++ Sorting Techniques/BubbleSort.java
@@ -1,12 +1,10 @@
 // Bubble Sorting...
 
-import java.util.Arrays;
-
 public class BubbleSort {
     public static void main(String[] args) {
         int[] arr = {1, 2, 3, 4, 5, 8, 6};
-        int ans = bubble(arr);
-        System.out.println(Arrays.toString(ans));
+        // int ans = bubble(arr);
+        // System.out.println(Arrays.toString(ans));
     }
 
     static void bubble(int[] arr) {
