1a72fc1ca17afbf19e10b42fdc73d4fca34e8621
Deeptansh Nagar <diptanshnagar@gmail.com>
2025-06-06 02:07:11 +0530

    dsa

---
+++ Sorting Techniques/FindDupDips.java
@@ -0,0 +1,31 @@
+// reliable method, solved by diptansh himself
+
+
+public class FindDupDips {
+    public static void main(String[] args) {
+        int[] nums = {3, 1, 3, 4, 2};
+        System.out.println(findDuplicate(nums));
+    }
+    public static int findDuplicate(int[] nums) {
+        int i = 0;
+        while(i < nums.length) {
+            int correctIndex = nums[i] - 1;
+            if(nums[i] != nums[correctIndex]) {
+                swap(nums, i, correctIndex);
+            } else {
+                i++;
+            }
+        }
+        for (int index = 0; index < nums.length; index++) {
+            if(nums[index] != index+1){
+                return nums[index];
+            }
+        }
+        return -1;
+    }
+    static void swap (int[] nums, int i, int correctIndex) {
+        int temp = nums[i];
+        nums[i] = nums[correctIndex];
+        nums[correctIndex] = temp;
+    }
+}
