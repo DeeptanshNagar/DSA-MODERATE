4565c805e81a67cc6350e78323a7fde9686ec95e
Deeptansh Nagar <diptanshnagar@gmail.com>
2025-06-05 23:43:45 +0530

    dsa

--- Sorting Techniques/testFindOLmissing.java
+++ Sorting Techniques/testFindOLmissing.java
@@ -6,13 +6,32 @@ import java.util.List;
 
 public class testFindOLmissing {
     public static void main(String[] args) {
-        int[] arr = {4,3,2,7,8,2,3,1};
-        FindAllMissing finder = new FindAllMissing();
+        int[] nums = {4,3,2,7,8,2,3,1};
+        List<Integer> result = findDisappearedNumbers(nums);
+        System.out.println("Missing Number: " + result);
 
     }
-    public List<Integer> findDisappearedNumbers(int[] nums) {
+    public static List<Integer> findDisappearedNumbers(int[] nums) {
         int i = 0;
-        while(i < list.length)
+        while(i < nums.length) {
+            int correctIndex = nums[i] - 1;
+            if(nums[i] != nums[correctIndex]) {
+                swap(nums, i, correctIndex);
+            } else {
+                i++;
+            }
         }
+        List<Integer> ans = new ArrayList<>();
+        for (int index = 0; index < nums.length; index++) {
+            if(nums[index] != index + 1){
+                ans.add(index + 1);
+            }
+        }
+        return ans;
+    }
+    static void swap(int[] nums, int i, int correctIndex){
+        int temp = nums[i];
+        nums[i] = nums[correctIndex];
+        nums[correctIndex] = temp;
     }
 }
