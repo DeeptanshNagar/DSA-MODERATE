9da43fb19cf0c12881d2767ac3060d6cc67aeb2f
Deeptansh Nagar <diptanshnagar@gmail.com>
2025-06-08 05:36:23 +0530

    patterns

--- Patterns/patterns.java
+++ Patterns/patterns.java
@@ -5,7 +5,9 @@ public class patterns {
         // pattern1(4);
         // pattern2(4);
         // pattern3();
-        pattern4(5);
+        // pattern4(5);
+        // pattern5(5);
+        pattern28(5);
     }
     // static void pattern1(int n) {
     //     for (int rows = 1; rows <= n; rows++) {
@@ -34,10 +36,34 @@ public class patterns {
     //     }
     // }
 
-    static void pattern4(int n) {
-        for (int rows = 1; rows <= n; rows++) {
-            for (int cols = 1; cols <= rows; cols++) {
-                System.out.print(cols + " ");
+    // static void pattern4(int n) {
+    //     for (int rows = 1; rows <= n; rows++) {
+    //         for (int cols = 1; cols <= rows; cols++) {
+    //             System.out.print(cols + " ");
+    //         }
+    //         System.out.println();
+    //     }
+    // }
+
+    // static void pattern5(int n) {
+    //     for (int row = 1; row < 2*n; row++) {
+    //         int totalColsInRow = row > n? 2*n-row:row; // n - (row-n)
+    //         for (int col = 1; col <= totalColsInRow; col++) {
+    //             System.out.print("* ");
+    //         }
+    //         System.out.println();
+    //     }
+    // }
+
+    static void pattern28(int n) {
+        for (int row = 0; row < 2*n; row++) {
+            int totalColsInRow = row>n? 2*n-row: row;
+            int totalNumberOfSpaces = n-totalColsInRow;
+            for (int s = 0; s < totalNumberOfSpaces; s++) {
+                System.out.print(" ");
+            }
+            for (int col = 0; col < totalColsInRow; col++) {
+                System.out.print("* ");
             }
             System.out.println();
         }
