3d4bae24ec5e087a660b3eb79bd9c392ff8c5d66
Deeptansh Nagar <diptanshnagar@gmail.com>
2025-06-27 23:39:30 +0530

    ds

--- Recursion/Patterns/pattern.java
+++ Recursion/Patterns/pattern.java
@@ -1,12 +1,30 @@
 public class pattern {
     public static void main(String[] args) {
-        pattern3(5);
+        reverseTriangle(4, 0);
+        Triangle(4, 0);
     }
-    static void pattern3(int n) {
-        for (int rows = 1; rows <= n; rows++) {
-            for (int cols = 1; cols <= n+1-rows; cols++) { // 5
-                System.out.print("* ");
-            }
+
+    static void reverseTriangle(int row, int col) {
+        if(row == 0) {
+            return;
+        }
+        if(col < row) {
+            System.out.print("* ");
+            reverseTriangle(row, col + 1);
+        } else {
+            System.out.println();
+            reverseTriangle(row - 1, 0);
+        }
+    }
+    static void Triangle(int row, int col) {
+        if(row == 0) {
+            return;
+        }
+        if(col < row) {
+            Triangle(row, col + 1);
+            System.out.print("* ");
+        } else {
+            Triangle(row - 1, 0);
             System.out.println();
         }
     }
