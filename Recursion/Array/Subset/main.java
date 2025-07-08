c1f21f0ed9e07616e630b990a2635e8ff6d184fc
Deeptansh Nagar <diptanshnagar@gmail.com>
2025-07-08 20:41:16 +0530

    dsa

--- Recursion/Array/Subset/main.java
+++ Recursion/Array/Subset/main.java
@@ -1,11 +1,13 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 
 public class main {
     public static void main(String[] args) {
-        int[] arr = {1,2,3};
-        List<List<Integer>> ans = subset(arr);
+        int[] arr = {1,2,2};
+        // List<List<Integer>> ans = subset(arr);
+        List<List<Integer>> ans = subsetDuplicates(arr);
         for(List<Integer> list : ans){
             System.out.println(list);
         }
@@ -27,7 +29,28 @@ public class main {
             // outer = {[], [1], [2], [1,2]} // after iteration
         }
         return outer;
+        // tc = o(n*2^n)
+        // sc = o(n*2^n)
+    }
+
+    static List<List<Integer>> subsetDuplicates(int[] arr) {
+        Arrays.sort(arr);
+        List<List<Integer>> outer = new ArrayList<>();
+        outer.add(new ArrayList<>());
+        int start = 0;
+        int end = 0;
+        for(int i = 0; i < arr.length; i++) { 
+            start = 0;
+            if(i > 0 && arr[i] == arr[i-1]){
+                start = end + 1;
+            }
+            end = outer.size() - 1;
+            for (int j = start; j <= end; j++) {
+                List<Integer> internal = new ArrayList<>(outer.get(j));
+                internal.add(arr[i]);
+                outer.add(internal);
+            }
+        }
+        return outer;
     }
 }
-// tc = o(n*2^n)
-// sc = o(n*2^n)
