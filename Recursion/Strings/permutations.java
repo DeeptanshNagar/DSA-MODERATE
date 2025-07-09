07533dcc2cc49153bf948ee11617083ecdc7fdf9
Deeptansh Nagar <diptanshnagar@gmail.com>
2025-07-09 23:05:06 +0530

    dsa

--- Recursion/Strings/permutations.java
+++ Recursion/Strings/permutations.java
@@ -4,11 +4,14 @@ import java.util.List;
 
 public class permutations {
     public static void main(String[] args) {
-        // combinations("", "abc");
-        List<String> ans = combinationsList("", "abc");
-        System.out.println(ans);
+        // permutation("", "abc");
+
+        // List<String> ans = permutationsList("", "abc");
+        // System.out.println(ans);
+
+        System.out.println(permutationCounts("", "abc"));
     }
-    static void combinations(String p, String up) {
+    static void permutation(String p, String up) {
         if(up.isEmpty()){
             System.out.println(p);
             return;
@@ -17,11 +20,11 @@ public class permutations {
         for (int i = 0; i <= p.length(); i++) {
             String f = p.substring(0, i);
             String s = p.substring(i, p.length());
-            combinations(f + ch + s, up.substring(1));
+            permutation(f + ch + s, up.substring(1));
         }
     }
 
-    static List<String> combinationsList(String p, String up) {
+    static List<String> permutationsList(String p, String up) {
         if(up.isEmpty()){
             List<String> list = new ArrayList<>();
             list.add(p);
@@ -34,8 +37,23 @@ public class permutations {
         for (int i = 0; i <= p.length(); i++) {
             String f = p.substring(0, i);
             String s = p.substring(i, p.length());
-            ans.addAll(combinationsList(f + ch + s, up.substring(1)));
+            ans.addAll(permutationsList(f + ch + s, up.substring(1)));
         }
         return ans;
+    }
+
+    static int permutationCounts(String p, String up) {
+        if(up.isEmpty()){
+            // System.out.println(p);
+            return 1;
+        }
+        int count = 0;
+        char ch = up.charAt(0);
+        for (int i = 0; i <= p.length(); i++) {
+            String f = p.substring(0, i);
+            String s = p.substring(i, p.length());
+            count = count + permutationCounts(f + ch + s, up.substring(1));
+        }
+        return count;
     }
 }
