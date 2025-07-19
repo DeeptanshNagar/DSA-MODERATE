bebb3268ce11fcdf8ce921963f39ffd01aa1c701
Deeptansh Nagar <diptanshnagar@gmail.com>
2025-07-20 04:32:01 +0530

    dsa

--- BackTracking/maze.java
+++ BackTracking/maze.java
@@ -18,12 +18,19 @@ public class maze {
         // System.out.println(answer);
 
         // Suppose a obstacle river is at index [2,2] and now we are starting from [0,0] index and we have to reach [3,3] index
+        // boolean[][] maze = {
+        //     {true, true, true},
+        //     {true, false, true},
+        //     {true, true, true}
+        // };
+        // mazeStepsRiver("", maze, 0, 0);
+
         boolean[][] maze = {
             {true, true, true},
-            {true, false, true},
+            {true, true, true},
             {true, true, true}
         };
-        mazeStepsRiver("", maze, 0, 0);
+        mazeStepsRiverBT("", maze, 0, 0);
     }
     static int maze(int row, int col) {
         if(row == 1 || col == 1) {
@@ -122,5 +129,34 @@ public class maze {
             mazeStepsRiver(p + "R", maze, row, col+1);
         }
     }
-       
+
+    static void mazeStepsRiverBT(String p, boolean[][] maze, int row, int col) {
+        if(row == maze.length - 1 && col == maze[0].length - 1) {
+            System.out.println(p);
+            return;
+        }
+
+        if(!maze[row][col]) {
+            return;
+        }
+
+        // i am considering this block in my path
+        maze[row][col] = false;
+
+        if(row < maze.length - 1) {
+            mazeStepsRiverBT(p + "D", maze, row+1, col);
+        }
+        if(col < maze[0].length - 1) {
+            mazeStepsRiverBT(p + "R", maze, row, col+1);
+        }
+        if(row > 0) {
+            mazeStepsRiverBT(p + "U", maze, row - 1, col);
+        }
+        if(col > 0) {
+            mazeStepsRiverBT(p + "L", maze, row, col - 1);
+        }
+        // this line is where the function will be over
+        // so before the function gets removed, also remove the changes that were made by that function
+        maze[row][col] = true;
+    }
 }
