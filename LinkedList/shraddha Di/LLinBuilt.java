b52661a8ebaf0d5316b263abb209a4d70a1d5b41
Deeptansh Nagar <diptanshnagar@gmail.com>
2025-07-23 03:06:10 +0530

    dsa

--- LinkedList/shraddha Di/LLinBuilt.java
+++ LinkedList/shraddha Di/LLinBuilt.java
@@ -1,5 +1,36 @@
+import java.util.LinkedList;
+
 public class LLinBuilt {
     public static void main(String[] args) {
-        
+        LinkedList<String> list = new LinkedList<>();
+        list.addFirst("Batman");
+        list.addFirst("Thor");
+        System.out.println(list);
+
+        list.addLast("Tarzan");         // add and addLast do the same work
+        System.out.println(list);
+
+        System.out.println(list.size());
+        System.out.println();
+
+        for (int i = 0; i < list.size(); i++) {
+            System.out.print(list.get(i) + " -> ");
+        }
+        System.out.println("NULL");
+        System.out.println();
+
+        list.removeFirst();
+        System.out.println(list);
+        for (int i = 0; i < list.size(); i++) {
+            System.out.print(list.get(i) + " -> ");
+        }
+        System.out.println("NULL");
+
+        list.removeLast();
+        System.out.println(list);
+        for (int i = 0; i < list.size(); i++) {
+            System.out.print(list.get(i) + " -> ");
+        }
+        System.out.println("NULL");
     }
 }
