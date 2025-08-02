85ee0413a5d8db62073ee5a698476afee1d738b6
Deeptansh Nagar <diptanshnagar@gmail.com>
2025-08-03 02:23:14 +0530

    reccurLL

--- LinkedList/Single LL/tempCodeRunnerFile.java
+++
@@ -1,6 +0,0 @@
-oundNode = list.find(9);
-        // if (foundNode != null) {
-        //     System.out.println("Found node with value: " + foundNode.value);
-        // } else {
-        //     System.out.println("Value not found in list");
-        // }
