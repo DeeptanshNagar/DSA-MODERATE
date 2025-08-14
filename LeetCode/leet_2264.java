088a4fffe5b0991e5afb08965a23b6a602b772c3
Deeptansh Nagar <diptanshnagar@gmail.com>
2025-08-14 18:20:12 +0530

    dsa

--- LeetCode/leet_2264.java
+++ LeetCode/leet_2264.java
@@ -1,10 +1,23 @@
 public class leet_2264 {
     public static void main(String[] args) {
         String str = "6777133339";
-        largest3digit(str);
-        System.out.println(str);
+        String res = largest3digit(str);
+        System.out.println(res);
     }
-    static void largest3digit(String str) {
-        
+    static String largest3digit(String str) {
+        String ans = "";
+        for (int i = 0; i < str.length() - 3; i++) {
+            char c1 = str.charAt(i);
+            char c2 = str.charAt(i + 1);
+            char c3 = str.charAt(i + 2);
+
+            if(c1 == c2 && c2 == c3) {
+                String candidate = "" + c1 + c2 + c3;
+                if(ans.equals("") || candidate.compareTo(ans) > 0) {
+                    ans = candidate;
+                }
+            }
+        }
+        return ans;
     }
 }
