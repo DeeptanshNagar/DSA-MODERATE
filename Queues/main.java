8515838ca211b3526f8b64ab6f6285787c31646c
Deeptansh Nagar <diptanshnagar@gmail.com>
2025-10-21 12:41:25 +0530

    rem

--- Queues/main.java
+++ Queues/main.java
@@ -1,4 +1,4 @@
-import java.util.LinkedList;
+import java.util.*;
 // As we know :-
 // Stack - A data structure that works on the LIFO (Last-In-First-Out) principle
 
@@ -8,17 +8,30 @@ import java.util.LinkedList;
 public class main {
     public static void main(String[] args) {
         LinkedList<Integer> stack = new LinkedList<>();
-        stack.addFirst(3);
-        stack.addFirst(2);
-        stack.addFirst(69);
-        stack.removeFirst();
+        stack.addFirst(3); // push
+        stack.addFirst(2); // push
+        stack.addFirst(69); // push
+        stack.removeFirst(); // pop
         // Here, Linked List is acting as a Stack.
 
         LinkedList<Integer> queue = new LinkedList<>();
-        queue.addLast(1);
-        queue.addLast(2);
-        queue.addLast(3);
+        queue.addLast(1); // Enqueue
+        queue.addLast(2); // Enqueue
+        queue.addLast(3); // Enqueue
         System.out.println(queue);
+        Integer i = queue.removeFirst();
+        System.out.println(queue); // Dequeue
+        System.out.println(queue.peek()); //getfirst() // front most element in the queue.
+        // Here, Linked List is acting as a Queue.
+
+        Queue<Integer> queue2 = new LinkedList<>();
+        queue2.add(1); // Enqueue
+        queue2.add(2); // Enqueue
+        queue2.add(3); // Enqueue
+        System.out.println(queue2);
+        Integer i = queue2.remove();
+        System.out.println(queue2); // Dequeue
+        System.out.println(queue2.peek()); // front most element in the queue.
     }
 
 }
