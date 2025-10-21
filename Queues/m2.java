68746a8ccc1881e310c9bda1f9242550fe4be19b
Deeptansh Nagar <diptanshnagar@gmail.com>
2025-10-21 22:51:10 +0530

    queue

--- Queues/m2.java
+++ Queues/m2.java
@@ -2,8 +2,19 @@ import java.util.*;
 
 public class m2 {
     public static void main(String[] args) {
+        // Create a Queue of Integers using LinkedList implementation
         Queue<Integer> list = new LinkedList<>();
+
+        list.add(5);
+        
         System.out.println(list.size());
+
+        // Removes and returns the head of the queue
+        // Throws NoSuchElementException if the queue is empty
         // System.out.println(list.remove());
+
+        // Removes and returns the head of the queue
+        // Returns null instead of throwing an exception if the queue is empty
+        // System.out.println(list.poll());
     }
 }
