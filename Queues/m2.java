b0c4ea8d6905efc6fe8f4d334071ea871ea8a3b5
Deeptansh Nagar <diptanshnagar@gmail.com>
2025-10-21 22:58:19 +0530

    queue

--- Queues/m2.java
+++ Queues/m2.java
@@ -2,19 +2,33 @@ import java.util.*;
 
 public class m2 {
     public static void main(String[] args) {
-        // Create a Queue of Integers using LinkedList implementation
-        Queue<Integer> list = new LinkedList<>();
+        // Create a Queue of Integers using Linkedqueue1 implementation
+        Queue<Integer> queue1 = new Linkedqueue1<>();
 
-        list.add(5);
-        
-        System.out.println(list.size());
+        queue1.add(5);
+        queue1.add(10);
+        queue1.add(15);
+        queue1.add(20);
+        System.out.println(queue1.size());
 
         // Removes and returns the head of the queue
         // Throws NoSuchElementException if the queue is empty
-        // System.out.println(list.remove());
+        // System.out.println(queue1.remove());
 
         // Removes and returns the head of the queue
         // Returns null instead of throwing an exception if the queue is empty
-        // System.out.println(list.poll());
+        // System.out.println(queue1.poll());
+
+        // Retrieves, but does not remove, the head of the queue
+        // Throws NoSuchElementException if the queue is empty
+        System.out.println(queue1.element());
+
+        // Retrieves, but does not remove, the head of the queue
+        // Returns null if the queue is empty
+        System.out.println(queue1.peek());
+
+
+        Queue<Integer>
+
     }
 }
